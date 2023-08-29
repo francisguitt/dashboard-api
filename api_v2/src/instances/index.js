47502f059748a3838514233286af4edc049cd460
francisguitt <francisguitt22209@gmail.com>
2023-08-29 19:46:47 -0300

    .

--- api_v2/src/instances/index.js
+++ api_v2/src/instances/index.js
@@ -1,18 +1,9 @@
-// import admin from 'firebase-admin';
-
-// // Verifica se estamos no ambiente de desenvolvimento
-// if (process.env.NODE_ENV === 'development') {
-//     const devServiceAccount = require('./dev-serviceAccountKey.json');
-//     admin.initializeApp({
-//         credential: admin.credential.cert(devServiceAccount)
-//     });
-// } else {
-//     admin.initializeApp();
-// }
 
 
 import admin from 'firebase-admin';
+
 import secret from '../keys/servicesAcountKey.json' assert{type: "json"};
+
 admin.initializeApp({
     credential: admin.credential.cert(secret)
 });
