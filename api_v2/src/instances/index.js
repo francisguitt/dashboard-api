63534b2e9f9a6242a1d0b2afae0af629b935b8b2
francisguitt <francisguitt22209@gmail.com>
2023-08-29 20:00:35 -0300

    .

--- api_v2/src/instances/index.js
+++ api_v2/src/instances/index.js
@@ -2,7 +2,8 @@
 
 import admin from 'firebase-admin';
 
-import secret from '../keys/servicesAcountKey.json' assert{type: "json"};
+// import secret from '../keys/servicesAcountKey.json' assert{type: "json"};
+import secret from '../keys/servicesAcountKey.json' ;
 
 admin.initializeApp({
     credential: admin.credential.cert(secret)
